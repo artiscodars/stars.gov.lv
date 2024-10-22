6c37b829e17ef63714ac843f4f0659e68a2a8e37
artis@igstudija.lv <artis@igstudija.lv>
2024-10-22 15:43:33 +0300

    eu logo

--- resources/views/includes/partners.blade.php
+++ resources/views/includes/partners.blade.php
@@ -1,20 +1,9 @@
 <div class="border-t border-gray-200">
     <div class="container mx-auto px-6">
         <div class="flex  flex-wrap justify-center items-center gap-10 py-8 ">
-            <div><img src="{{ asset('images/viaa-logo.svg') }}" alt="Valsts izglītības attīstības aģentūra"
-                    class="h-16">
-            </div>
-            <div><img src="{{ asset('images/viaa-logo.svg') }}" alt="Valsts izglītības attīstības aģentūra"
-                    class="h-16">
-            </div>
-            <div><img src="{{ asset('images/viaa-logo.svg') }}" alt="Valsts izglītības attīstības aģentūra"
-                    class="h-16">
+            <div><img src="{{ asset('images/1.png') }}" alt="Valsts izglītības attīstības aģentūra" class=" h-40">
             </div>
 
-            <div><img src="{{ asset('images/viaa-logo.svg') }}" alt="Valsts izglītības attīstības aģentūra"
-                    class="h-162">
-
-            </div>
 
         </div>
     </div>
