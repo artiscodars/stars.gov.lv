fcff5afda5131c95c2bedbe8fcc9d8af7fdbcc31
artis@igstudija.lv <artis@igstudija.lv>
2024-11-29 15:17:28 +0200

    pieklusamibas poga

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -12,8 +12,25 @@
 
                     </div>
 
+
+
                     <!-- Language and Accessibility Options -->
-                    <div class="flex space-x-6 items-end">
+                    <div class="flex space-x-6 items-center">
+
+                        <!-- Accessibility Widget Open Button -->
+                        <div class="asw-widget">
+                            <a href="#" class="asw-menu-btn" title="Open Accessibility Menu" role="button"
+                                aria-expanded="false">
+                                <!-- Button Icon SVG -->
+                                <svg xmlns="http://www.w3.org/2000/svg" style="fill: white" viewBox="0 0 24 24"
+                                    width="30px" height="30px">
+                                    <path d="M0 0h24v24H0V0z" fill="none" />
+                                    <path d="M20.5 6c-2.61.7-5.67 1-8.5 1s-5.89-.3-8.5-1L3 8c1.86.5 4 .83 6 1v13h2v-6h2v6h2V9c2-.17 4.14-.5 6-1l-.5-2zM12
+           6c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z" />
+                                </svg>
+                            </a>
+                        </div>
+
 
                         <!-- Language Switch -->
                         <div class="flex gap-2 text-lg items-end leading-none">
@@ -66,7 +83,8 @@
 
 <script>
     function toggleNavMenu() {
-        const navMenu = document.getElementById("navMenu");
+        const
+            navMenu = document.getElementById("navMenu");
         if (navMenu.classList.contains("translate-x-[100%]")) {
             navMenu.classList.remove("translate-x-[100%]");
             navMenu.classList.add("translate-x-0");
