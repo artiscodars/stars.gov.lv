d47d80300156d8358ea571c3048444c8a344497d
artis@igstudija.lv <artis@igstudija.lv>
2024-11-30 06:33:30 +0200

    kartites

--- resources/views/pages/komponentes_kartites.blade.php
+++ resources/views/pages/komponentes_kartites.blade.php
@@ -33,37 +33,37 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
             @include('includes.card-programma', [
-    'severity' => 'primary',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'primary',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma', [
-    'severity' => 'secondary',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'secondary',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma', [
-    'severity' => 'third',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'third',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma', [
-    'severity' => 'success',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'success',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
             @include('includes.card-programma', [
-    'severity' => 'success-dark',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'success-dark',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
             @include('includes.card-programma', [
-    'severity' => 'warning',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'warning',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma', [
-    'severity' => 'error',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'error',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
         </div>
     </example>
 
@@ -76,37 +76,44 @@
         <div class="grid grid-cols-1 gap-4">
 
             @include('includes.card-programma-list', [
-    'severity' => 'primary',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'primary',
+            'veids' => 'Attālināti',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma-list', [
-    'severity' => 'secondary',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'secondary',
+            'veids' => 'Attālināti',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma-list', [
-    'severity' => 'third',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'third',
+            'veids' => 'Klātienē',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma-list', [
-    'severity' => 'success',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'success',
+            'veids' => 'Attālināti',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
             @include('includes.card-programma-list', [
-    'severity' => 'success-dark',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'success-dark',
+            'veids' => 'Klātienē',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
             @include('includes.card-programma-list', [
-    'severity' => 'warning',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'warning',
+            'veids' => 'Klātienē',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
 
             @include('includes.card-programma-list', [
-    'severity' => 'error',
-    'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
-])
+            'severity' => 'error',
+            'veids' => 'Attālināti',
+            'title' => '2.līmenis “Pamata digitālās pašapkalpošanās prasmes” (tālmācības programma)'
+            ])
         </div>
     </example>
 
@@ -117,22 +124,22 @@
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             @include('includes.card-iestade', [
-    'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)',
-    'stars' => 5,
-    'kosultants' => 'Anna Kalniņa'
-])
+            'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)',
+            'stars' => 5,
+            'kosultants' => 'Anna Kalniņa'
+            ])
 
             @include('includes.card-iestade', [
-    'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)',
-    'stars' => 3,
-    'kosultants' => 'Jānis Bērziņš'
-])
+            'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)',
+            'stars' => 3,
+            'kosultants' => 'Jānis Bērziņš'
+            ])
 
             @include('includes.card-iestade', [
-    'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)',
-    'stars' => 5,
-    'kosultants' => 'Anna kalniņa'
-])
+            'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)',
+            'stars' => 5,
+            'kosultants' => 'Anna kalniņa'
+            ])
 
 
 
@@ -146,20 +153,20 @@
     <example class="not-prose">
         <div class="grid grid-cols-1 gap-4">
             @include('includes.card-tests', [
-    'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
-])
+            'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
+            ])
 
             @include('includes.card-tests', [
-    'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
-])
+            'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
+            ])
 
             @include('includes.card-tests', [
-    'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
-])
+            'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
+            ])
 
             @include('includes.card-tests', [
-    'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
-])
+            'title' => 'Latvijas valsts mācību centrs "Studijas centrs" (tālmācības programma)'
+            ])
         </div>
     </example>
 
